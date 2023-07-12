7935a931ca62e8ed4e01ced50e26f92dd13a74d4
user <samuel_morgan_@outlook.com>
2023-07-12 18:57:07 -0300

    added tailwind and blackletter font

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Start</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+        sagittis et ante ut lacinia. Donec porta metus vel erat egestas, non
+        feugiat sapien bibendum. Nunc magna ex, tempus et volutpat vitae, congue
+        vel orci. Cras sem felis, scelerisque id accumsan sit amet, interdum sed
+        sapien. Praesent id mi ac purus sollicitudin dignissim a in urna. Morbi
+        laoreet faucibus neque, et porta dolor tristique sit amet. Proin rutrum
+        ut dui molestie malesuada. Nullam aliquet ut libero ullamcorper rhoncus.
+        Donec sodales, leo sit amet sodales venenatis, quam odio sagittis
+        tortor, vel tincidunt ex purus eget tellus. Vivamus in turpis dui. Sed
+        elementum odio id pulvinar vulputate. Curabitur et mattis neque.
+        Curabitur scelerisque ut dui at vulputate. Vivamus vel ligula
+        scelerisque, pulvinar lorem at, porttitor libero.
+      </p>
+    </div>
+  );
+}
+
+export default App;
