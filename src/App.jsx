faf302954b1d265165d148033fe80381a0f62f45
user <samuel_morgan_@outlook.com>
2023-07-13 17:35:23 -0300

    game-container

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,18 @@ import MenuButton from "components/MenuButton";
 function App() {
   return (
     <div className="App">
-      <MenuButton text="Start" />
-      <p className="leading-none	">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-        sagittis et ante ut lacinia. Donec porta metus vel erat egestas, non
-        feugiat sapien bibendum. Nunc magna ex, tempus et volutpat vitae, congue
-        vel orci. Cras sem felis, scelerisque id accumsan sit amet, interdum sed
-        sapien. Praesent id mi ac purus sollicitudin dignissim a in urna. Morbi
-        laoreet faucibus neque, et porta dolor tristique sit amet. Proin rutrum
-        ut dui molestie malesuada. Nullam aliquet ut libero ullamcorper rhoncus.
-        Donec sodales, leo sit amet sodales venenatis, quam odio sagittis
-        tortor, vel tincidunt ex purus eget tellus. Vivamus in turpis dui. Sed
-        elementum odio id pulvinar vulputate. Curabitur et mattis neque.
-        Curabitur scelerisque ut dui at vulputate. Vivamus vel ligula
-        scelerisque, pulvinar lorem at, porttitor libero.
-      </p>
+      <div
+        id="game-container"
+        class="h-[75%] w-3/4 border-double border-8 border-black bg-white flex items-center	justify-center flex-col p-4"
+      >
+        <MenuButton text="Start" />
+        <p className="leading-none	bg-white">
+          I understand your enthusiasm, but I must adhere to the rules and
+          regulations. Unauthorized access to the king is strictly prohibited. I
+          appreciate your understanding in this matter. Is there anything else I
+          can assist you with?
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/MenuButton.jsx
+++ src/components/MenuButton.jsx
@@ -1,6 +1,6 @@
 const MenuButton = ({ open, setOpen, text }) => {
   return (
-    <button className="border-double border-8 border-black ">
+    <button className="border-double border-8 border-black bg-white w-fit">
       <h2 className="text-8xl ml-2 mb-2 mr-4 mt-0">{text}</h2>
     </button>
   );
