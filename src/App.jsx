3455cc7b749cc3e0d6a5b335c713b2ad962c649b
user <samuel_morgan_@outlook.com>
2023-07-13 17:49:59 -0300

    basic screen nav + castle pics

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,50 @@
 import "App.css";
 import MenuButton from "components/MenuButton";
+import { useState } from "react";
 
 function App() {
+  const [screenID, setScreenID] = useState(0);
+  let currentScreen = null;
+  switch (screenID) {
+    case 0:
+      currentScreen = (
+        <>
+          <MenuButton text="Start" onClick={() => setScreenID(1)} />
+        </>
+      );
+      break;
+    case 1:
+      currentScreen = (
+        <>
+          screen2
+          <MenuButton text="Previous" onClick={() => setScreenID(0)} />
+          <MenuButton text="Next" onClick={() => setScreenID(2)} />
+        </>
+      );
+      break;
+    default:
+      currentScreen = (
+        <>
+          <MenuButton text="Previous" onClick={() => setScreenID(1)} />
+          <p className="leading-none	bg-white">
+            I understand your enthusiasm, but I must adhere to the rules and
+            regulations. Unauthorized access to the king is strictly prohibited.
+            I appreciate your understanding in this matter. Is there anything
+            else I can assist you with?
+          </p>
+        </>
+      );
+      break;
+  }
+
   return (
     <div className="App">
+      {screenID}
       <div
         id="game-container"
         class="h-[75%] w-3/4 border-double border-8 border-black bg-white flex items-center	justify-center flex-col p-4"
       >
-        <MenuButton text="Start" />
-        <p className="leading-none	bg-white">
-          I understand your enthusiasm, but I must adhere to the rules and
-          regulations. Unauthorized access to the king is strictly prohibited. I
-          appreciate your understanding in this matter. Is there anything else I
-          can assist you with?
-        </p>
+        {currentScreen}
       </div>
     </div>
   );
--- src/components/MenuButton.jsx
+++ src/components/MenuButton.jsx
@@ -1,6 +1,9 @@
-const MenuButton = ({ open, setOpen, text }) => {
+const MenuButton = ({ onClick, text }) => {
   return (
-    <button className="border-double border-8 border-black bg-white w-fit">
+    <button
+      onClick={onClick}
+      className="border-double border-8 border-black bg-white w-fit"
+    >
       <h2 className="text-8xl ml-2 mb-2 mr-4 mt-0">{text}</h2>
     </button>
   );
