74a2c0eed4c239e8bc7d81c65592c30e6f69a7f0
user <samuel_morgan_@outlook.com>
2023-07-17 18:02:55 -0300

    paragraph font increased

--- src/components/Paragraph.jsx
+++ src/components/Paragraph.jsx
@@ -1,6 +1,6 @@
 const Paragraph = ({ children, className }) => {
   return (
-    <p className={`${className} text-base leading-nonebg-white`}>{children}</p>
+    <p className={`${className} text-2xl leading-nonebg-white`}>{children}</p>
   );
 };
 
