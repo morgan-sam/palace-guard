7c609326a84e082604590b7dc979ff270e1882b0
user <samuel_morgan_@outlook.com>
2023-07-17 18:04:17 -0300

    ... button works again

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ const Button = ({ className, disabled, onClick, text }) => {
     <button
       disabled={disabled}
       onClick={onClick}
-      className={`border-double border-4 border-black bg-white w-fit ${className}`}
+      className={`border-double border-4 border-black bg-white ${className}`}
     >
       <h2 className="text-4xl ml-1 mb-1 mr-2 mt-0">{text}</h2>
     </button>
